a90a97d04ce2639fd12920f97b6ec3721e2d6526
lcssoaress <lcssoaresmachado@gmail.com>
2025-11-27 20:21:20 -0300

    prim commit

--- app/src/main/java/br/com/etecia/myapp/Theforest.java
+++ app/src/main/java/br/com/etecia/myapp/Theforest.java
@@ -3,4 +3,23 @@ package br.com.etecia.myapp;
 public class Theforest {
     private String titulo;
     private int imgweapon;
+
+    public Theforest() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getImgweapon() {
+        return imgweapon;
+    }
+
+    public void setImgweapon(int imgweapon) {
+        this.imgweapon = imgweapon;
+    }
 }
